ffbb5483eceef5678f19f2ea49cadbd6aa2e3b87
Peter Tessing Petersen <tessing@gmail.com>
2019-01-16 17:18:01 +0100

    Added first Project

---
+++ Part 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+    class askAndPrint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write your name");
+
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + UserName);
+        }
+    }
+
